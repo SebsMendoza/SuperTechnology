1714ca941beffb3cfa62592f7287d9e8d70c45d6
Sebastián Mendoza Acosta <jhoansebastian.mendoza@alumno.iesluisvives.org>
2023-04-22 18:19:22 +0200

    Commit final

---
+++ usuarios-api/src/main/kotlin/resa/rodriguez/services/storage/IStorageService.kt
@@ -0,0 +1,23 @@
+package resa.rodriguez.services.storage
+
+import org.springframework.core.io.Resource
+import org.springframework.web.multipart.MultipartFile
+import java.nio.file.Path
+import java.util.stream.Stream
+
+/**
+ * Interface for the Storage Service.
+ * @author Mario Resa, Daniel Rodriguez, Jhoan Sebastian Mendoza,
+ * Alfredo Rafael Maldonado, Azahara Blanco, Ivan Azagra, Roberto Blazquez
+ */
+interface IStorageService {
+    fun initStorageDirectory()
+    fun store(file: MultipartFile): String
+    fun loadAll(): Stream<Path>
+    fun load(fileName: String): Path
+    fun loadAsResource(fileName: String): Resource
+    fun delete(fileName: String)
+    fun deleteAll()
+    fun getUrl(fileName: String): String
+    suspend fun storeFileFromUser(file: MultipartFile, fileName: String): String
+}
